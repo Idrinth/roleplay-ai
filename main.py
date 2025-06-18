e786cb4c4924f404d9602ae8c7998670d9f357e6
Björn Büttner <Idrinth@users.noreply.github.com>
2025-06-18 15:21:23 +0200

    fix message list

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fastapi import FastAPI
 import mariadb
 from redis import Redis
 from pymongo import MongoClient
+from pydantic import BaseModel
 
 
 def simplify_result(query_result: QueryResponse):
@@ -27,13 +28,19 @@ mdbconn = mariadb.connect(
     port=3306
 )
 
+
+
+class Action(BaseModel):
+    description: str | None = None
+
+
 @app.get("/")
 def index():
     return {"message": "Hello, World!"}
 
 
 @app.put("/chat/{uuid}")
-def chat(uuid: str, body: str):
+def chat(uuid: str, action: Action):
     search_result = qdrant.query(
         collection_name=uuid,
         query_text=body,
@@ -46,34 +53,40 @@ def chat(uuid: str, body: str):
     long_term_summary = redis.get(uuid + ".long_text_summary") or ""
     medium_term_summary = redis.get(uuid + ".medium_text_summary") or ""
     short_term_summary = redis.get(uuid + ".short_text_summary") or ""
+    messages = [
+        {
+            "role": "agent",
+            "content": "Personality:\n\nYou are a game master. React to provided texts with in character responses and try to progress the story appropriately taking the provided context into account."
+                       "\n\n"
+                       "Short Term Summary:\n\n" + short_term_summary +
+                       "\n\n"
+                       "Medium Term Summary:\n\n" + medium_term_summary +
+                       "\n\n"
+                       "Long Term Summary:\n\n" + long_term_summary +
+                       "\n\n"
+                       "Further Information:\n\n" + json.dumps(result),
+        },
+    ]
     try:
         cursor = mdbconn.cursor()
         cursor.execute("CREATE DATABASE `"+uuid+"` IF NOT EXISTS")
         cursor.execute("USE DATABASE `"+uuid+"`")
         cursor.execute(
             "CREATE TABLE IF NOT EXISTS  messages {aid BIGINT NOT NULL autoincrement, creator char(5),content text, PRIMARY KEY aid} charset=utf8;")
-        messages = cursor.execute("SELECT creator, content FROM messages LIMIT 20 SORT BY aid DESC").fetchall()
+        old_messages = cursor.execute("SELECT creator, content FROM messages LIMIT 20 SORT BY aid DESC").fetchall()
+        for message in old_messages:
+            messages += {
+                "role": message["creator"],
+                "content": message["content"],
+            }
     except Exception as e:
         print(e)
+    messages += {
+        "role": "user",
+        "content": "Action:\n\n" + action.description,
+    }
     response: ChatResponse = ollama.chat(
         model="gemma3",
-        messages=[
-            {
-                "role": "agent",
-                "content": "Personality:\n\nYou are a game master. React to provided texts with in character responses and try to progress the story appropriately taking the provided context into account."
-                           "\n\n"
-                           "Short Term Summary:\n\n" + short_term_summary +
-                           "\n\n"
-                           "Medium Term Summary:\n\n" + medium_term_summary +
-                           "\n\n"
-                           "Long Term Summary:\n\n" + long_term_summary +
-                           "\n\n"
-                           "Further Information:\n\n" + json.dumps(result),
-            },
-            {
-                "role": "user",
-                "content": "Action:\n\n" + body,
-            },
-        ]
+        messages=messages
     )
     return {"message": response["message"]["content"]}
