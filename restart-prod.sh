cbc404c33405bd316743f1b4dd95c88b2c43108f
Björn Büttner <Idrinth@users.noreply.github.com>
2025-07-02 11:56:31 +0200

    update restart script

--- restart-prod.sh
+++ restart-prod.sh
@@ -1,1 +1,1 @@
-git pull && docker compose -f compose.prod.yml down && docker-compose -f compose.prod.yml up -d --build && service nginx stop && service nginx start
+git pull && docker compose -f compose.prod.yml down && docker-compose -f compose.prod.yml up -d --build && sudo service nginx stop && sudo service nginx start
