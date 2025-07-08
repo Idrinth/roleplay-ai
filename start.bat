6f97fe9f61f3093c932bd8ceaf435e51a91448c0
Björn Büttner <Idrinth@users.noreply.github.com>
2025-07-08 09:28:19 +0200

    remove unused dev version

--- start.bat
+++
@@ -1,3 +0,0 @@
-docker-compose -f compose.dev.yml up ui -d
-sleep 10
-start http://127.0.0.1
--- train.bat
+++
@@ -1,2 +0,0 @@
-docker-compose -f compose.dev.yml build training
-docker-compose -f compose.dev.yml up training -d
