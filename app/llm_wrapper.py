3016814aded3a9b0b90cce7b90cf2c5a66203135
Björn Büttner <Idrinth@users.noreply.github.com>
2025-07-30 19:04:21 +0200

    add beam support to speed up deployed answers

---
+++ app/llm_wrapper.py
@@ -0,0 +1,48 @@
+import os
+import re
+import string
+from typing import Dict, List
+
+import requests
+
+llm_model = os.getenv('LLM_MODEL')
+beam_url = os.getenv('BEAM_API_KEY')
+beam_key = os.getenv('BEAM_DEPLOYMENT_URL')
+llm_to_use = os.getenv('LLM_TO_USE')
+
+def ask_llm(messages: List[Dict[string, string]]):
+    if llm_to_use == "beam":
+        response = requests.post(
+            beam_url,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {beam_key}",
+            },
+            json={
+                "messages": messages,
+            }
+        )
+
+        if response.status_code == 200:
+            response_content = response.json()["answer"]
+            response_content = re.sub("^(\n|.)*</think>\\s*", "", response_content).strip()
+
+            return response_content
+    elif llm_to_use == "local":
+        response = requests.post(
+            "http://llama:8000/v1/chat/completions",
+            headers={
+                "Content-Type": "application/json"
+            },
+            json={
+                "model": llm_model,
+                "messages": messages,
+            }
+        )
+
+        if response.status_code == 200:
+            response_content = response.json()["choices"][0]["message"]["content"]
+            response_content = re.sub("^(\n|.)*</think>\\s*", "", response_content).strip()
+
+            return response_content
+    raise ValueError("Could not get response from LLM")
---
+++ beam/gmai.py
@@ -0,0 +1,39 @@
+from beam import endpoint, Image, QueueDepthAutoscaler, Volume
+
+CACHE_PATH = "./weights"
+
+def download_models():
+    from transformers import AutoTokenizer, AutoModelForCausalLM
+    model = AutoModelForCausalLM.from_pretrained("teknium/OpenHermes-2.5-Mistral-7B", cache_dir=CACHE_PATH)
+    tokenizer = AutoTokenizer.from_pretrained("teknium/OpenHermes-2.5-Mistral-7B", cache_dir=CACHE_PATH)
+
+    return model, tokenizer
+
+@endpoint(
+    name="gamemaster-ai",
+    on_start=download_models,
+    volumes=[Volume(name="gamemaster-ai-cache", mount_path=CACHE_PATH)],
+    cpu=1,
+    gpu=["A100-40", "H100"],
+    memory="4Gi",
+    autoscaler=QueueDepthAutoscaler(
+        max_containers=5,
+        tasks_per_container=1,
+    ),
+    keep_warm_seconds=90,
+    authorized=True,
+    image=Image(python_version="python3.12", python_packages="requirements.remote.txt", env_vars="HF_HUB_ENABLE_HF_TRANSFER=1"),
+)
+def answer(context, **params):
+    model, tokenizer = context.on_start_value
+
+    text = tokenizer.apply_chat_template(params["messages"], tokenize=True, add_generation_prompt=True, return_tensors="pt")
+    generate_ids = model.generate(text, max_length=550)
+    result = tokenizer.batch_decode(
+        generate_ids,
+        skip_special_tokens=True,
+        clean_up_tokenization_spaces=True
+    )[0]
+
+    parts = result.split("\n assistant\n")
+    return {"answer": parts[len(parts) - 1]}
