8afc15e0f6edcb81f36909a358db48d1690c2b8e
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-02 21:35:07 +0200

    add async handling (experimental!) (#44)

--- app/llm_wrapper.py
+++ app/llm_wrapper.py
@@ -2,6 +2,7 @@ import os
 import re
 import string
 from typing import Dict, List
+import aiohttp
 
 import requests
 
@@ -10,39 +11,41 @@ beam_url = os.getenv('BEAM_DEPLOYMENT_URL')
 beam_key = os.getenv('BEAM_API_KEY')
 llm_to_use = os.getenv('LLM_TO_USE')
 
-def ask_llm(messages: List[Dict[string, string]]):
+async def ask_llm(messages: List[Dict[string, string]]):
     if llm_to_use == "beam":
-        response = requests.post(
-            beam_url,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": f"Bearer {beam_key}",
-            },
-            json={
-                "messages": messages,
-            }
-        )
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                beam_url,
+                headers={
+                    "Content-Type": "application/json",
+                    "Authorization": f"Bearer {beam_key}",
+                },
+                json={
+                    "messages": messages,
+                }
+            ) as response:
 
-        if response.status_code == 200:
-            response_content = response.json()["answer"]
-            response_content = re.sub("^(\n|.)*</think>\\s*", "", response_content).strip()
+                if response.status_code == 200:
+                    response_content = response.json()["answer"]
+                    response_content = re.sub("^(\n|.)*</think>\\s*", "", response_content).strip()
 
-            return response_content
+                    return response_content
     elif llm_to_use == "local":
-        response = requests.post(
-            "http://llama:8000/v1/chat/completions",
-            headers={
-                "Content-Type": "application/json"
-            },
-            json={
-                "model": llm_model,
-                "messages": messages,
-            }
-        )
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                "http://llama:8000/v1/chat/completions",
+                headers={
+                    "Content-Type": "application/json"
+                },
+                json={
+                    "model": llm_model,
+                    "messages": messages,
+                }
+            ) as response:
 
-        if response.status_code == 200:
-            response_content = response.json()["choices"][0]["message"]["content"]
-            response_content = re.sub("^(\n|.)*</think>\\s*", "", response_content).strip()
+                if response.status_code == 200:
+                    response_content = response.json()["choices"][0]["message"]["content"]
+                    response_content = re.sub("^(\n|.)*</think>\\s*", "", response_content).strip()
 
-            return response_content
+                    return response_content
     raise ValueError("Could not get response from LLM")
