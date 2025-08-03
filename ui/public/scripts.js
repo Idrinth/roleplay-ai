4447b431d208f9b15a0256d1b125221f1c1a0efd
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-03 11:45:31 +0200

    Update scripts.js

--- ui/public/scripts.js
+++ ui/public/scripts.js
@@ -384,7 +384,7 @@
           document.getElementById('chat').lastElementChild.classList.add(message.role);
         }
         if (json.messages.length === 0 && !document.getElementById('chat-entry').value) {
-          const value = await fetch(`/starting-point-proposal`, {
+          const value = await fetch(`${apiHost}/starting-point-proposal`, {
             method: 'POST',
             headers: {
               'Accept': 'application/json',
