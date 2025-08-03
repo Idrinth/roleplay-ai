3ff2fd37651036d3768724140baf3766e667f169
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-03 11:03:46 +0200

    Add reconnect ping (#76)

--- app/main.py
+++ app/main.py
@@ -344,8 +344,9 @@ async def chat_delete(chat_id: str, user_jwt: Annotated[str | None, Cookie()] = 
         return {"error": "Not a valid User"}
     if not is_uuid_like(chat_id):
         return {"error": "Not a valid Chat"}
+    sql_connection.ping()
     sql_connection.cursor().execute(f"DROP DATABASE `{mariadb_name(user_id, chat_id)}`;")
-    sql_connection.cursor().execute(f"DELETE FROM chat_users.mapping WHERE user_id='{user_id}' and chat_id='{chat_id}';")
+    sql_connection.cursor().execute("DELETE FROM chat_users.mapping WHERE user_id=? and chat_id=?;", [user_id, chat_id])
     redis.delete(f"{user_id}-{chat_id}.active")
     redis.delete(f"{user_id}-{chat_id}.short_summary")
     redis.delete(f"{user_id}-{chat_id}.medium_summary")
