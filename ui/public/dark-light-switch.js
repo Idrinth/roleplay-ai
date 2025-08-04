27ae711579d6820035543e2b3261ef832ecbe454
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-04 12:35:50 +0200

    peerlist badge will now follow the light/dark color switch

--- ui/public/dark-light-switch.js
+++ ui/public/dark-light-switch.js
@@ -1,13 +1,16 @@
 (async() => {
   if (window.matchMedia && !window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.getElementsByTagName('html')[0]?.classList.toggle('inverted-colors');
+    document.getElementsByTagName('html')[0]?.classList.add('inverted-colors');
+    document.getElementById('peerlist')?.firstElementChild?.setAttribute('src', 'https://peerlist.io/api/v1/projects/embed/PRJHGNQ86JOPGEP7RF8E6QDP769BEN?showUpvote=true&theme=light');
   }
   if (window.localStorage) {
     const preferedColorScheme = window.localStorage.getItem('prefered-color-scheme');
     if (preferedColorScheme === 'dark') {
       document.getElementsByTagName('html')[0]?.classList.remove('inverted-colors');
+    document.getElementById('peerlist')?.firstElementChild?.setAttribute('src', 'https://peerlist.io/api/v1/projects/embed/PRJHGNQ86JOPGEP7RF8E6QDP769BEN?showUpvote=true&theme=dark')
     } else if (preferedColorScheme === 'light') {
       document.getElementsByTagName('html')[0]?.classList.add('inverted-colors');
+    document.getElementById('peerlist')?.firstElementChild?.setAttribute('src', 'https://peerlist.io/api/v1/projects/embed/PRJHGNQ86JOPGEP7RF8E6QDP769BEN?showUpvote=true&theme=light')
     }
     window.localStorage.setItem('prefered-color-scheme', document.getElementsByTagName('html')[0]?.classList.contains('inverted-colors') ? 'light' : 'dark');
   }
@@ -17,5 +20,7 @@
       'prefered-color-scheme',
       document.getElementsByTagName('html')[0]?.classList.contains('inverted-colors') ? 'light' : 'dark',
     );
+
+    document.getElementById('peerlist')?.firstElementChild?.setAttribute('src', 'https://peerlist.io/api/v1/projects/embed/PRJHGNQ86JOPGEP7RF8E6QDP769BEN?showUpvote=true&theme=' + (document.getElementsByTagName('html')[0]?.classList.contains('inverted-colors') ? 'light' : 'dark'));
   }
 })();
