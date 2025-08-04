8fd14762eae2d04e575c641c39a573d2321dcfda
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-04 14:05:30 +0200

    Prepare eslint usage in frontend (#117)

---
+++ ui/eslint.config.js
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+
+export default defineConfig([
+	{
+		files: ["**/*.js"],
+		languageOptions: {
+			ecmaVersion: 2022,
+			globals: {
+				...globals.browser,
+        jsyaml: true,
+        showdown: true,
+			},
+		},
+    plugins: {
+			js,
+		},
+    linterOptions: {
+			noInlineConfig: true,
+		},
+		extends: ["js/recommended"],
+	},
+]);
---
+++ ui/tools/builder.js
@@ -0,0 +1,34 @@
+import {readdirSync, writeFileSync, readFileSync, mkdirSync, existsSync, rmSync} from 'fs';
+import {minify} from 'minify';
+
+if (existsSync(process.cwd() + '/dist')) {
+  rmSync(process.cwd() + '/dist', {recursive: true});
+}
+mkdirSync(process.cwd()+'/dist');
+const config = JSON.parse(readFileSync(process.cwd() + '/.minify.json', 'utf8'))
+
+for(const file of readdirSync(process.cwd() + '/public', 'utf-8')){
+  if (file.endsWith('.js') || file.endsWith('.html') || file.endsWith('.css')) {
+    try {
+      const data = await minify(process.cwd() + '/public/' + file, config);
+      if (data) {
+        writeFileSync(process.cwd() + '/dist/' + file, data);
+      } else {
+        console.error(`Failed to minify: ${file}`);
+        writeFileSync(process.cwd() + '/dist/' + file, readFileSync(process.cwd() + '/public/' + file, 'binary'), 'binary');
+      }
+    } catch (e) {
+      console.error(`Failed to minify: ${file}`);
+      writeFileSync(process.cwd() + '/dist/' + file, readFileSync(process.cwd() + '/public/' + file, 'binary'), 'binary');
+    }
+  } else {
+    writeFileSync(process.cwd() + '/dist/'+file, readFileSync(process.cwd() +'/public/'+file, 'binary'), 'binary');
+  }
+}
+
+for (const library of [
+  'node_modules/js-yaml/dist/js-yaml.min.js',
+  'node_modules/showdown/dist/showdown.min.js',
+]) {
+  writeFileSync(process.cwd() + '/dist/'+library.replace(/\\/g, '/').split('/').pop(), readFileSync(library, 'utf8'), 'utf8');
+}
