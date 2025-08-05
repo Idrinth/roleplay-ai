52600443739c5c15b3d21b148e4650372cbbe91b
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-05 19:26:02 +0200

    fix minification by removing regexp

---
+++ ui/public/modals.js
@@ -0,0 +1,62 @@
+bjoernbuettner = bjoernbuettner ||{};
+((attachTo) => {
+  attachTo.prompt = async (text, defaultText = '') => {
+    return new Promise(resolve => {
+      const prmt = document.createElement('div');
+      prmt.setAttribute('id', 'prompt');
+      prmt.appendChild(document.createElement('label'));
+      prmt.firstElementChild.appendChild(document.createTextNode(text));
+      prmt.appendChild(document.createElement('input'));
+      prmt.lastElementChild.value = defaultText;
+      prmt.lastElementChild.onchange = () => {
+        prmt.lastElementChild.disabled = !prmt.lastElementChild.previousElementSibling.value;
+      }
+      prmt.appendChild(document.createElement('button'));
+      prmt.lastElementChild.appendChild(document.createTextNode('Send'));
+      prmt.lastElementChild.onclick = () => {
+        if (!prmt.lastElementChild.previousElementSibling.value) {
+          return;
+        }
+        document.body.removeChild(prmt);
+        resolve(prmt.lastElementChild.previousElementSibling.value);
+      }
+      document.body.appendChild(prmt);
+    });
+  }
+  attachTo.confirm = async (text) => {
+    return new Promise(resolve => {
+      const prmt = document.createElement('div');
+      prmt.setAttribute('id', 'prompt');
+      prmt.appendChild(document.createElement('p'));
+      prmt.firstElementChild.appendChild(document.createTextNode(text));
+      prmt.appendChild(document.createElement('button'));
+      prmt.lastElementChild.appendChild(document.createTextNode('Yes'));
+      prmt.lastElementChild.onclick = () => {
+        document.body.removeChild(prmt);
+        resolve(true);
+      }
+      prmt.appendChild(document.createElement('button'));
+      prmt.lastElementChild.appendChild(document.createTextNode('No'));
+      prmt.lastElementChild.onclick = () => {
+        document.body.removeChild(prmt);
+        resolve(false);
+      }
+      document.body.appendChild(prmt);
+    });
+  }
+  attachTo.alert = async (text) => {
+    return new Promise(resolve => {
+      const prmt = document.createElement('div');
+      prmt.setAttribute('id', 'prompt');
+      prmt.appendChild(document.createElement('p'));
+      prmt.firstElementChild.appendChild(document.createTextNode(text));
+      prmt.appendChild(document.createElement('button'));
+      prmt.lastElementChild.appendChild(document.createTextNode('OK'));
+      prmt.lastElementChild.onclick = () => {
+        document.body.removeChild(prmt);
+        resolve();
+      }
+      document.body.appendChild(prmt);
+    });
+  }
+})(bjoernbuettner);
--- ui/tools/builder.js
+++ ui/tools/builder.js
@@ -1,11 +1,26 @@
-import {readdirSync, writeFileSync, readFileSync, mkdirSync, existsSync, rmSync} from 'fs';
+import {readdirSync, writeFileSync, readFileSync, mkdirSync, existsSync, rmSync} from 'node:fs';
 import {minify} from 'minify';
+import { hash } from 'node:crypto';
 
 if (existsSync(process.cwd() + '/dist')) {
   rmSync(process.cwd() + '/dist', {recursive: true});
 }
 mkdirSync(process.cwd()+'/dist');
 const config = JSON.parse(readFileSync(process.cwd() + '/.minify.json', 'utf8'))
+
+const TO_MERGE = {
+  'shared-##.min.js': [
+    'dark-light-switch.js',
+    'modals.js',
+    'paypal-donate-sdk.min.js',
+    'paypal.js',
+  ],
+  'chat-##.min.js': [
+    'showdown.min.js',
+    'js-yaml.min.js',
+    'chat.js'
+  ]
+}
 
 for(const file of readdirSync(process.cwd() + '/public', 'utf-8')){
   if (file.endsWith('.js') || file.endsWith('.html') || file.endsWith('.css')) {
@@ -18,7 +33,7 @@ for(const file of readdirSync(process.cwd() + '/public', 'utf-8')){
         writeFileSync(process.cwd() + '/dist/' + file, readFileSync(process.cwd() + '/public/' + file, 'binary'), 'binary');
       }
     } catch (e) {
-      console.error(`Failed to minify: ${file}`);
+      console.error(`Failed to minify ${file}: `, e);
       writeFileSync(process.cwd() + '/dist/' + file, readFileSync(process.cwd() + '/public/' + file, 'binary'), 'binary');
     }
   } else {
@@ -34,3 +49,25 @@ for (const library of [
 }
 
 writeFileSync(process.cwd() + '/dist/paypal-donate-sdk.min.js', await (await fetch('https://www.paypalobjects.com/donate/sdk/donate-sdk.js')).text(), 'utf8');
+
+for (const target of Object.keys(TO_MERGE)) {
+  let out = [];
+  for (const file of TO_MERGE[target]) {
+    out.push(readFileSync(`${process.cwd()}/dist/${file}`, 'utf8'));
+    rmSync(`${process.cwd()}/dist/${file}`);
+  }
+  const content = out.join('\n');
+  const contentHash = hash('md5', content, 'hex');
+  const name = target.replace('##', contentHash);
+  writeFileSync(`${process.cwd()}/dist/${name}`, content, 'utf8');
+  for(const file of readdirSync(process.cwd() + '/dist', 'utf-8')){
+    if (file.endsWith('.html')) {
+      writeFileSync(
+        `${process.cwd()}/dist/${file}`,
+        readFileSync(`${process.cwd()}/dist/${file}`, 'utf8')
+          .replace(`/${target}`, `/${name}`),
+        'utf8'
+      );
+    }
+  }
+}
