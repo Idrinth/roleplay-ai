f5754bf7d41145d213e1f42ad05e6deb6215e00f
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-05 12:18:27 +0200

    make the UI slightly less horrid

---
+++ ui/public/list-existing-chats.js
@@ -0,0 +1,45 @@
+(async() => {
+  const apiHost = location.protocol + '//' + location.hostname + '/api/v1';
+  const user = await (async () => {
+    const user = await (await fetch(`${apiHost}/whoami`, {
+      credentials: "include",
+      method: "GET",
+    })).json();
+    if (!user.error && !user.exception) {
+      return user;
+    }
+    return undefined;
+  })();
+  if (!user) {
+    return;
+  }
+  for (const achat of user.chats) {
+    document.getElementById('worlds').appendChild(document.createElement('li'));
+    const world = document.getElementById('worlds').lastElementChild;
+    world.appendChild(document.createElement('a'));
+    world.lastElementChild.innerHTML = achat.name;
+    world.lastElementChild.setAttribute('href', '/chat/' + achat.id);
+    world.lastElementChild.setAttribute('data-id', achat.id);
+    world.appendChild(document.createElement('span'));
+    world.lastElementChild.appendChild(document.createTextNode('[D]'));
+    world.lastElementChild.classList.add('button');
+    world.lastElementChild.setAttribute('title', 'Delete chat');
+    world.lastElementChild.onclick = async () => {
+      if (await confirm(`Do you want to delete ${achat.name}?`)) {
+        await fetch(
+          `${apiHost}/chat/${achat.id}/delete`,
+          {
+            credentials: "include",
+            method: "POST",
+            signal: AbortSignal.timeout(10000),
+          }
+        );
+        if (chat.id === achat.id) {
+          window.location = location.protocol + '//' + location.hostname + '/chat';
+          return;
+        }
+        document.getElementById('worlds').removeChild(world);
+      }
+    }
+  }
+})();
