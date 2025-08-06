ce5370e0fde35207daeec3367e377e947b96200e
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-06 08:55:44 +0200

    remove async

--- app/chat_active.py
+++ app/chat_active.py
@@ -7,6 +7,8 @@ def chat_is_in_use(user_id: str, chat_id: str):
 
 def set_chat_unused(user_id: str, chat_id: str):
     redis.set(f"{user_id}-{chat_id}.{ACTIVE_KEY}", "false")
+    redis.expire(f"{user_id}-{chat_id}.{ACTIVE_KEY}", 600)
 
 def set_chat_in_use(user_id: str, chat_id: str):
     redis.set(f"{user_id}-{chat_id}.{ACTIVE_KEY}", "true")
+    redis.expire(f"{user_id}-{chat_id}.{ACTIVE_KEY}", 600)
