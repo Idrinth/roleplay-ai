654e4918577160413f9ce5a8904c3834caeb146e
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-06 08:18:16 +0200

    Async redis (#138)

--- app/chat_active.py
+++ app/chat_active.py
@@ -5,8 +5,8 @@ ACTIVE_KEY="chat_is_active"
 def chat_is_in_use(user_id: str, chat_id: str):
     return redis.get(f"{user_id}-{chat_id}.{ACTIVE_KEY}") == "true"
 
-def set_chat_unused(user_id: str, chat_id: str):
-    redis.set(f"{user_id}-{chat_id}.{ACTIVE_KEY}", "false")
+async def set_chat_unused(user_id: str, chat_id: str):
+    await redis.set(f"{user_id}-{chat_id}.{ACTIVE_KEY}", "false")
 
-def set_chat_in_use(user_id: str, chat_id: str):
-    redis.set(f"{user_id}-{chat_id}.{ACTIVE_KEY}", "true")
+async def set_chat_in_use(user_id: str, chat_id: str):
+    await redis.set(f"{user_id}-{chat_id}.{ACTIVE_KEY}", "true")
