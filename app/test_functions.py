3917df5b3eb22d240390e748be54b96d16d89b2d
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-06 19:01:08 +0200

    add new test

--- app/test_functions.py
+++ app/test_functions.py
@@ -12,6 +12,12 @@ class FunctionTestCase(unittest.TestCase):
                 "c303282d-f2e6-46ca-a04a-35d3d873712f"
             ),
             'c303282df2e646caa04a35d3d873712dc303282df2e646caa04a35d3d873712f')
+    def test_is_uuid_like(self):
+        self.assertFalse(is_uuid_like(None))
+        self.assertFalse(is_uuid_like(""))
+        self.assertFalse(is_uuid_like("sfdfdsfdsfsf"))
+        self.assertTrue(is_uuid_like(uuid.UUID().hex))
+        self.assertTrue(is_uuid_like(uuid.uuid4().hex))
 
 if __name__ == '__main__':
     unittest.main()
