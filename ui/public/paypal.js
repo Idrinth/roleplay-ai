c2cf7bdab6fd6f7d6901d786fa01c2a63500ab91
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-06 15:13:23 +0200

    add accessibility improvements (#149)

--- ui/public/paypal.js
+++ ui/public/paypal.js
@@ -4,8 +4,14 @@
     hosted_button_id: 'AENZ9E63F85G8',
     image: {
       src: 'https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif',
-      alt: 'Donate with PayPal button',
+      alt: 'Donate with PayPal',
       title: 'PayPal - The safer, easier way to pay online!',
     }
   }).render('#donate-button');
+  setTimeout(() => {
+    const paypal = document.getElementById('donate-button');
+    paypal.setAttribute('tabindex', '0');
+    paypal.setAttribute('role', 'button');
+    paypal.setAttribute('aria-label', 'opens in a new window');
+  }, 0);
 })();
