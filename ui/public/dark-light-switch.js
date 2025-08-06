df77386ae88700dab0e58de4bc5d480fcee1b390
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-06 18:52:12 +0200

    fix dark/light switching and add meta description (#155)

--- ui/public/dark-light-switch.js
+++ ui/public/dark-light-switch.js
@@ -1,26 +1,28 @@
 (async() => {
-  if (window.matchMedia && !window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.getElementsByTagName('html')[0]?.classList.add('inverted-colors');
-    document.getElementById('peerlist')?.firstElementChild?.setAttribute('src', 'https://peerlist.io/api/v1/projects/embed/PRJHGNQ86JOPGEP7RF8E6QDP769BEN?showUpvote=true&theme=light');
-  }
-  if (window.localStorage) {
-    const preferedColorScheme = window.localStorage.getItem('prefered-color-scheme');
-    if (preferedColorScheme === 'dark') {
-      document.getElementsByTagName('html')[0]?.classList.remove('inverted-colors');
-    document.getElementById('peerlist')?.firstElementChild?.setAttribute('src', 'https://peerlist.io/api/v1/projects/embed/PRJHGNQ86JOPGEP7RF8E6QDP769BEN?showUpvote=true&theme=dark')
-    } else if (preferedColorScheme === 'light') {
-      document.getElementsByTagName('html')[0]?.classList.add('inverted-colors');
-    document.getElementById('peerlist')?.firstElementChild?.setAttribute('src', 'https://peerlist.io/api/v1/projects/embed/PRJHGNQ86JOPGEP7RF8E6QDP769BEN?showUpvote=true&theme=light')
+  const setMode = (colorMode) => {
+    if (!colorMode) {
+      colorMode = 'dark';
     }
-    window.localStorage.setItem('prefered-color-scheme', document.getElementsByTagName('html')[0]?.classList.contains('inverted-colors') ? 'light' : 'dark');
+    if (colorMode === 'dark') {
+      document.getElementsByTagName('html')[0]?.classList.remove('inverted-colors');
+    } else {
+      document.getElementsByTagName('html')[0]?.classList.add('inverted-colors');
+    }
+    document.getElementById('peerlist')?.firstElementChild?.setAttribute(
+      'src',
+      'https://peerlist.io/api/v1/projects/embed/PRJHGNQ86JOPGEP7RF8E6QDP769BEN?showUpvote=true&theme=' + colorMode
+    );
+    window?.localStorage?.setItem('preferred-color-scheme', colorMode);
+    return colorMode;
+  }
+  let prefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+  if (window.localStorage) {
+    prefersLight = setMode(window.localStorage.getItem('preferred-color-scheme') ?? (prefersLight ? 'light' : 'dark')) === 'light';
+  } else if(prefersLight) {
+    setMode('light');
   }
   document.getElementById('logo').onclick = () => {
-    document.getElementsByTagName('html')[0]?.classList.toggle('inverted-colors');
-    window.localStorage && window.localStorage.setItem(
-      'prefered-color-scheme',
-      document.getElementsByTagName('html')[0]?.classList.contains('inverted-colors') ? 'light' : 'dark',
-    );
-
-    document.getElementById('peerlist')?.firstElementChild?.setAttribute('src', 'https://peerlist.io/api/v1/projects/embed/PRJHGNQ86JOPGEP7RF8E6QDP769BEN?showUpvote=true&theme=' + (document.getElementsByTagName('html')[0]?.classList.contains('inverted-colors') ? 'light' : 'dark'));
+    prefersLight = !prefersLight;
+    setMode(prefersLight ? 'light' : 'dark');
   }
 })();
