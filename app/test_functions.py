c6735b5e4a9ce2078318c3f5462c84d6d30dd6d6
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-06 19:02:21 +0200

    add first tests (#156)

---
+++ app/test_functions.py
@@ -0,0 +1,23 @@
+import unittest
+from app.functions import *
+
+class FunctionTestCase(unittest.TestCase):
+    def test_get_rules(self):
+        self.assertTrue(len(get_rules()) > 0)
+    def test_mariadb_name(self):
+        self.assertRaises(TypeError, mariadb_name, "mar-i-a","---db")
+        self.assertEqual(
+            mariadb_name(
+                "c303282d-f2e6-46ca-a04a-35d3d873712d",
+                "c303282d-f2e6-46ca-a04a-35d3d873712f"
+            ),
+            'c303282df2e646caa04a35d3d873712dc303282df2e646caa04a35d3d873712f')
+    def test_is_uuid_like(self):
+        self.assertFalse(is_uuid_like(None))
+        self.assertFalse(is_uuid_like(""))
+        self.assertFalse(is_uuid_like("sfdfdsfdsfsf"))
+        self.assertTrue(is_uuid_like(uuid.UUID().hex))
+        self.assertTrue(is_uuid_like(uuid.uuid4().hex))
+
+if __name__ == '__main__':
+    unittest.main()
