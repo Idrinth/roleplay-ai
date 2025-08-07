ba4eda2e230034160bd92f42b5e809ae67dab9f7
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-07 05:25:00 +0200

    add missing plural s (#165)

--- ui/public/document-upload.js
+++ ui/public/document-upload.js
@@ -10,10 +10,10 @@
     const upload = async() => {
       const id = element.getAttribute('data-id');
       if (id && element.value && element.getAttribute('data-raw') !== element.value && await root.confirm(`Do you want to save this modified ${resource}?`)) {
-        return await root.getFromAPI(`chat/${chatId}/${resource}/${id}`, 'POST', getBody(element));
+        return await root.getFromAPI(`chat/${chatId}/${resource}s/${id}`, 'POST', getBody(element));
       }
       if (element.value && await root.confirm(`Do you want to save this new ${resource}?`)) {
-        return await root.getFromAPI(`chat/${chatId}/${resource}`, 'POST', getBody(element));
+        return await root.getFromAPI(`chat/${chatId}/${resource}s`, 'POST', getBody(element));
       }
     }
     await upload();
