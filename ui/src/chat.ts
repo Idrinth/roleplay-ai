9d4680304bec9a2815cbb77c760447fc9e5310ba
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-08 23:19:33 +0200

    fix the first few typescript issues

--- ui/src/chat.ts
+++ ui/src/chat.ts
@@ -97,131 +97,126 @@
 
   const sendButton = document.getElementById('send') as  null|HTMLButtonElement;
   const loader = document.getElementById('loader');
-  if (sendButton) {
-    window.setInterval(async () => {
-      const response = await root.getFromAPI(
-        `chat/${chat.id}/active?${Date.now()}`,
-        'GET',
-        null,
-        2400,
-      );
-      if (typeof response !== 'object' || response === null || !Object.hasOwn(response, 'active')) {
-        return;
-      }
-      sendButton.disabled = (response as {active: boolean}).active;
-      loader?.setAttribute('style', sendButton.disabled ? '' : 'display: none');
-    }, 2500);
-  }
   const documents = document.getElementById('documents');
-  if (documents) {
-    const updateDocuments = async () => {
-      const json = await root.getFromAPI(`chat/${chat.id}/documents`, 'GET');
-      while (documents.children.length > 2) {
-        const lastElementChild = documents.lastElementChild;
-        if (lastElementChild) {
-          documents.removeChild(lastElementChild);
-        }
-      }
-      // @ts-ignore
-      if (!json || typeof json !== 'object' || Object.hasOwn(json, 'documents') || !Array.isArray(json.documents)) {
-        for (const md_document of (json as {documents: {name: string,content: string, id: string}[]}).documents) {
-          const doc = document.createElement('li');
-          documents.appendChild(doc);
-          const docName = document.createElement('span');
-          doc.appendChild(docName);
-          docName.appendChild(document.createTextNode(md_document.name));
-          doc.appendChild(root.button('[E]', 'Edit document', (event: MouseEvent) => {
-            event.stopPropagation();
-            const el = document.createElement('textarea');
-            el.setAttribute('id', 'document')
-            el.setAttribute('data-id', md_document.id);
-            el.setAttribute('data-name', md_document.name);
-            el.value = md_document.content;
-            el.setAttribute('data-raw', md_document.content);
-            document.body.appendChild(el);
-          }));
-          doc.appendChild(root.button('[D]', 'Delete document', async (event: MouseEvent) => {
-            event.stopPropagation();
-            if (await root.confirm("Do you want to delete this document?")) {
-              await root.getFromAPI(`chat/${chat.id}/characters/${md_document.id}/delete`, 'POST');
-              await updateDocuments();
-            }
-          }));
-        }
-      }
-    }
-  }
   const characters = document.getElementById('characters');
-  if (characters) {
-    const updateCharacters = async () => {
-      const json = await root.getFromAPI(`chat/${chat.id}/characters`, 'GET');
-      while (characters.children.length > 1) {
-        const character = characters.lastElementChild;
-        if (character) {
-          characters.removeChild(character);
-        }
-      }
-      // @ts-ignore
-      if (!json || typeof json !== 'object' || !Object.hasOwn(json, 'characters') || !Array.isArray(json.characters)) {
-        for (const character of (json as { characters: {id: string, name: {taken: string}}[] }).characters) {
-          const characterElement = document.createElement('li');
-          characters.appendChild(characterElement);
-          characterElement.appendChild(document.createElement('span'));
-          characterElement.lastElementChild?.appendChild(document.createTextNode(character.name.taken));
-          characterElement.appendChild(root.button('[E]', 'Edit character', async (event: MouseEvent) => {
-            event.stopPropagation();
-            const el = document.createElement('textarea');
-            el.setAttribute('id', 'charactersheet')
-            const char = {...character} as {id?: string, name: {taken: string}};
-            delete char['id'];
-            el.setAttribute('data-id', character.id);
-            el.value = window?.jsyaml?.dump(char) ?? '';
-            el.setAttribute('data-raw', el.value);
-            document.body.appendChild(el);
-          }));
-          characterElement.appendChild(root.button('[D]', 'Delete character', async (event: MouseEvent) => {
-            event.stopPropagation();
-            if (await root.confirm("Do you want to delete this character sheet?")) {
-              await root.getFromAPI(`chat/${chat.id}/characters/${character.id}/delete`, 'POST');
-              await updateCharacters();
-            }
-          }));
-        }
-      }
-    }
-  }
+  const world = document.getElementById('world') as HTMLInputElement|null;
   const chatWrapper = document.getElementById('chat');
   const chatEntry = document.getElementById('chat-entry') as HTMLTextAreaElement|null;
-  if (sendButton && chatWrapper && chatEntry && window?.showdown?.Converter) {
-    sendButton.addEventListener('click', async function () {
-      const value = chatEntry.value;
-      if (!value || !value.trim()) {
-        return;
-      }
-      // @ts-ignore
-      const converter = new window.showdown.Converter();
-      chatEntry.value = '';
-      const chatElement = document.createElement('li');
-      chatWrapper.appendChild(chatElement);
-      chatElement.innerHTML = converter.makeHtml(value);
-      chatElement.classList.add('user');
-      const json = await root.getFromAPI(`chat/${chat.id}`, 'POST', {description: value}) as Promise<{message?: string, error?: string, exception?: string}>;
-      if (typeof json === 'object' && Object.hasOwn(json, 'message')) {
-        const message = json.message as string;
-        const reply = document.createElement('li');
-        chatWrapper.appendChild(reply);
-        reply.innerHTML = '<span class="gamemaster"></span>' + converter.makeHtml(message);
-        reply.classList.add('agent');
-      }
-    });
+  if (!sendButton || !documents || !characters || !world || !chatWrapper || !chatEntry || !window?.showdown?.Converter) {
+    return;
   }
+  const converter = new window.showdown.Converter();
+  window.setInterval(async () => {
+    const response = await root.getFromAPI(
+      `chat/${chat.id}/active?${Date.now()}`,
+      'GET',
+      null,
+      2400,
+    );
+    if (typeof response !== 'object' || response === null || !Object.hasOwn(response, 'active')) {
+      return;
+    }
+    sendButton.disabled = (response as {active: boolean}).active;
+    loader?.setAttribute('style', sendButton.disabled ? '' : 'display: none');
+  }, 2500);
+  const updateDocuments = async () => {
+    const json = await root.getFromAPI(`chat/${chat.id}/documents`, 'GET');
+    while (documents.children.length > 2) {
+      const lastElementChild = documents.lastElementChild;
+      if (lastElementChild) {
+        documents.removeChild(lastElementChild);
+      }
+    }
+    // @ts-ignore
+    if (!json || typeof json !== 'object' || Object.hasOwn(json, 'documents') || !Array.isArray(json.documents)) {
+      for (const md_document of (json as {documents: {name: string,content: string, id: string}[]}).documents) {
+        const doc = document.createElement('li');
+        documents.appendChild(doc);
+        const docName = document.createElement('span');
+        doc.appendChild(docName);
+        docName.appendChild(document.createTextNode(md_document.name));
+        doc.appendChild(root.button('[E]', 'Edit document', (event: MouseEvent) => {
+          event.stopPropagation();
+          const el = document.createElement('textarea');
+          el.setAttribute('id', 'document')
+          el.setAttribute('data-id', md_document.id);
+          el.setAttribute('data-name', md_document.name);
+          el.value = md_document.content;
+          el.setAttribute('data-raw', md_document.content);
+          document.body.appendChild(el);
+        }));
+        doc.appendChild(root.button('[D]', 'Delete document', async (event: MouseEvent) => {
+          event.stopPropagation();
+          if (await root.confirm("Do you want to delete this document?")) {
+            await root.getFromAPI(`chat/${chat.id}/characters/${md_document.id}/delete`, 'POST');
+            await updateDocuments();
+          }
+        }));
+      }
+    }
+  }
+  const updateCharacters = async () => {
+    const json = await root.getFromAPI(`chat/${chat.id}/characters`, 'GET');
+    while (characters.children.length > 1) {
+      const character = characters.lastElementChild;
+      if (character) {
+        characters.removeChild(character);
+      }
+    }
+    // @ts-ignore
+    if (!json || typeof json !== 'object' || !Object.hasOwn(json, 'characters') || !Array.isArray(json.characters)) {
+      for (const character of (json as { characters: {id: string, name: {taken: string}}[] }).characters) {
+        const characterElement = document.createElement('li');
+        characters.appendChild(characterElement);
+        characterElement.appendChild(document.createElement('span'));
+        characterElement.lastElementChild?.appendChild(document.createTextNode(character.name.taken));
+        characterElement.appendChild(root.button('[E]', 'Edit character', async (event: MouseEvent) => {
+          event.stopPropagation();
+          const el = document.createElement('textarea');
+          el.setAttribute('id', 'charactersheet')
+          const char = {...character} as {id?: string, name: {taken: string}};
+          delete char['id'];
+          el.setAttribute('data-id', character.id);
+          el.value = window?.jsyaml?.dump(char) ?? '';
+          el.setAttribute('data-raw', el.value);
+          document.body.appendChild(el);
+        }));
+        characterElement.appendChild(root.button('[D]', 'Delete character', async (event: MouseEvent) => {
+          event.stopPropagation();
+          if (await root.confirm("Do you want to delete this character sheet?")) {
+            await root.getFromAPI(`chat/${chat.id}/characters/${character.id}/delete`, 'POST');
+            await updateCharacters();
+          }
+        }));
+      }
+    }
+  }
+  sendButton.addEventListener('click', async function () {
+    const value = chatEntry.value;
+    if (!value || !value.trim()) {
+      return;
+    }
+    chatEntry.value = '';
+    const chatElement = document.createElement('li');
+    chatWrapper.appendChild(chatElement);
+    chatElement.innerHTML = converter.makeHtml(value);
+    chatElement.classList.add('user');
+    const json = await root.getFromAPI(`chat/${chat.id}`, 'POST', {description: value}) as {message?: string, error?: string, exception?: string};
+    if (typeof json === 'object' && Object.hasOwn(json, 'message')) {
+      const message = (json as {message: string}).message;
+      const reply = document.createElement('li');
+      chatWrapper.appendChild(reply);
+      reply.innerHTML = '<span class="gamemaster"></span>' + converter.makeHtml(message);
+      reply.classList.add('agent');
+    }
+  });
   let handlingClick = false;
   document.body.onclick = async (event) => {
     if (handlingClick) {
       return;
     }
     handlingClick = true;
-    await root.uploadDocument(event, chat.id, 'character', async(element) => window.jsyaml.load(element.value), updateCharacters);
+    await root.uploadDocument(event, chat.id, 'character', async(element) => window?.jsyaml?.load(element.value), updateCharacters);
     await root.uploadDocument(event, chat.id, 'document', async(element) => {
       return {
         content: element.value,
@@ -231,60 +226,60 @@
     handlingClick = false;
   }
   await updateCharacters();
-  document.getElementById('add-character').onclick = async (event) => {
+  document.getElementById('add-character')?.addEventListener('click', async (event) => {
     event.stopPropagation();
     const el = document.createElement('textarea');
     el.setAttribute('id', 'character');
     el.value = characterFiller;
     document.body.appendChild(el);
-  }
+  });
   await updateDocuments();
-  document.getElementById('add-document').onclick = async (event) => {
+  document.getElementById('add-document')?.addEventListener('click', async (event) => {
     event.stopPropagation();
     const el = document.createElement('textarea');
     el.setAttribute('id', 'document');
     el.value = '';
     document.body.appendChild(el);
-  }
+  });
   await (async () => {
     const response = await root.getFromAPI(`chat/${chat.id}/world`,'GET');
-    if (!response.world) {
+    if (typeof response !== 'object' || response === null || !Object.hasOwn(response, 'world')) {
       return;
     }
-    const keywords = response.world;
-    document.getElementById('world').previousElementSibling.setAttribute('title', keywords.join("\n"))
-    document.getElementById('world').setAttribute('data-original', JSON.stringify(keywords))
-    document.getElementById('world').value = keywords.join(", ")
+    const keywords = (response as {world: string[]}).world;
+    world?.previousElementSibling?.setAttribute('title', keywords.join("\n"))
+    world.setAttribute('data-original', JSON.stringify(keywords))
+    world.value = keywords.join(", ")
   })();
-  document.getElementById("world").onchange = async() => {
-    const keywords = document.getElementById('world').value.split(",").map((keyword) => {
+  world.addEventListener('change', async() => {
+    const keywords = world.value.split(",").map((keyword) => {
       return keyword.trim()
     }).filter((keyword) => {
       return !!keyword;
     });
     keywords.sort();
-    const oldKeywords = JSON.parse(document.getElementById('world').getAttribute('data-original')).sort();
+    const oldKeywords = JSON.parse(world.getAttribute('data-original') ?? '[]').sort();
     if (keywords.join() === oldKeywords.join()) {
       return;
     }
-    document.getElementById('world').setAttribute('data-original', JSON.stringify(keywords))
-    document.getElementById('world').previousElementSibling.setAttribute('title', keywords.join("\n"))
+    world.setAttribute('data-original', JSON.stringify(keywords))
+    world?.previousElementSibling?.setAttribute('title', keywords.join("\n"))
     await root.getFromAPI(`chat/${chat.id}/world`, 'PUT', {
       keywords,
     });
-  }
+  });
   await (async () => {
     const json = await root.getFromAPI(`chat/${chat.id}`, 'GET');
-    const converter = new window.showdown.Converter();
-    if (json.messages) {
-      for (const message of json.messages) {
-        document.getElementById('chat').appendChild(document.createElement('li'));
-        document.getElementById('chat').lastElementChild.innerHTML = (message.role === 'agent' ? '<span class="gamemaster"></span>' : '') + converter.makeHtml(message.content);
-        document.getElementById('chat').lastElementChild.classList.add(message.role);
+    if (typeof json === 'object' && json !== null && Object.hasOwn(json, 'messages') && Array.isArray((json as {messages: any}).messages)) {
+      for (const message of (json as {messages: {role: string, content: string}[]}).messages) {
+        const listElement = document.createElement('li');
+        chatWrapper.appendChild(listElement);
+        listElement.innerHTML = (message.role === 'agent' ? '<span class="gamemaster"></span>' : '') + converter.makeHtml(message.content);
+        listElement.classList.add(message.role);
       }
-      if (json.messages.length === 0 && !document.getElementById('chat-entry').value && await root.confirm('Do you want help with your beginning scene?')) {
-        const world = await root.prompt("What is the world like? Please provide keywords separated by comma.");
-        document.getElementById('world').value = world;
+      if (((json as {messages?: []})?.messages ?? [])?.length === 0 && !chatEntry.value && await root.confirm('Do you want help with your beginning scene?')) {
+        const keywords = await root.prompt("What is the world like? Please provide keywords separated by comma.");
+        world.value = keywords;
         const value = await root.getFromAPI(`chat/${chat.id}/starting-point-proposal`, 'POST', {
           name: await root.prompt("What is your character's name?"),
           race: await root.prompt("What is your character's race?"),
@@ -296,9 +291,9 @@
           mood: await root.prompt("What is your character's mood?"),
           weather: await root.prompt("What is your weather like?"),
           genre: await root.prompt("What genre does the world fall into?"),
-          world,
+          world: keywords,
         });
-        document.getElementById('chat-entry').value = value.message;
+        chatEntry.value = typeof value === 'object' && value !== null && Object.hasOwn(value, 'message') ? (value as {message: string})?.message : '';
       }
     }
   })();
