d021bbb668c49b038e8a6318322e6aa3bf6209e2
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-10 00:53:04 +0200

    introduce element size of the element under focus (#190)

---
+++ ui/src/on-hover-focus.ts
@@ -0,0 +1,26 @@
+(() => {
+  const footer = document.createElement('footer');
+  const worlds = document.createElement('worlds');
+  const documents = document.createElement('documents');
+  const characters = document.createElement('characters');
+  const content = document.getElementById('content');
+  if (!content) {
+    return;
+  }
+  const elements = {footer, worlds, documents, characters};
+  for (const element of Object.keys(elements)) {
+    if (Object.hasOwn(elements, element)) {
+      const item = elements[element as keyof typeof elements] as HTMLElement;
+      for (const event of ['mouseenter', 'focusin']) {
+        item.addEventListener(event, () => {
+          content.classList.add(`hovers-${element}`);
+        });
+      }
+      for (const event of ['mouseleave', 'focusout']) {
+        item.addEventListener(event, () => {
+          content.classList.remove(`hovers-${element}`);
+        });
+      }
+    }
+  }
+})();
