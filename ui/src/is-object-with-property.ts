6317388b2e1d32e25f8e8eff70bc2988e611b57c
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-10 20:21:12 +0200

    fix any -> unknown

--- ui/src/is-object-with-property.ts
+++ ui/src/is-object-with-property.ts
@@ -1,5 +1,5 @@
 ((root) => {
-  root.isObjectWithProperty = (value: any, key: string): boolean => {
+  root.isObjectWithProperty = (value: unknown, key: string): boolean => {
     return typeof value === 'object' && value !== null && Object.hasOwn(value, key);
   }
 })(window.bjoernbuettner)
