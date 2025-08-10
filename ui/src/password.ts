91947f957b8c9c1e4a4cfcf68d462e5f88fd9063
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-11 00:50:33 +0200

    minification no longer breaks the password generator

--- ui/src/password.ts
+++ ui/src/password.ts
@@ -1,6 +1,7 @@
 (async(root) => {
   const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-.:,;+*~#<>!§$%&/()?='.split('');
-  const randChar = () => chars[Math.floor(Math.random() * chars.length)] as string;
+  const random = () => Math.random() * chars.length;
+  const randChar = () => chars[Math.floor(random())] as string;
   root.password = (minChars: number = 8, maxChars: number = 24) => {
     if (minChars > maxChars) {
       maxChars = minChars;
@@ -8,6 +9,7 @@
     let out = '';
     const length = minChars + Math.floor(Math.random() * (maxChars - minChars));
     for (let i = 0; i < length; i++) {
+      chars.sort(() => Math.random() > 0.5 ? -1 : 1);
       out += randChar();
     }
     return out;
