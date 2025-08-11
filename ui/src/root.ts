0d4257c200f1c2c1a8b3770fe9179bfccdba7be0
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-11 10:55:48 +0200

    add audio support

--- ui/src/root.ts
+++ ui/src/root.ts
@@ -1,6 +1,7 @@
 declare global {
   interface Window {
     bjoernbuettner: {
+      audios: {src: string, keyword: string}[],
       isObjectWithProperty: <K extends PropertyKey>(value: unknown, key: string) => value is object & Record<K, unknown>,
       button: (text: string, title: string, callback: (ev: MouseEvent) => void, useActualButton: boolean) => HTMLButtonElement|HTMLSpanElement,
       alert: (text: string) => Promise<void>,
--- ui/tools/builder.js
+++ ui/tools/builder.js
@@ -7,6 +7,7 @@ if (existsSync(process.cwd() + '/dist')) {
   rmSync(process.cwd() + '/dist', {recursive: true});
 }
 mkdirSync(process.cwd()+'/dist');
+mkdirSync(process.cwd()+'/dist/audio');
 const config = JSON.parse(readFileSync(process.cwd() + '/.minify.json', 'utf8'))
 
 const TO_MERGE = {
@@ -28,8 +29,27 @@ const TO_MERGE = {
     'document-upload.js',
     'chat.js',
     'on-hover-focus.js',
+    'audio-list.js',
+    'audio.js'
   ]
 }
+
+const audios = [];
+for (const keyword of readdirSync(process.cwd() + '/audio')) {
+  mkdirSync(process.cwd() + '/dist/audio/' + keyword);
+  for (const file of readdirSync(process.cwd() + '/audio/' + keyword)) {
+    audios.push({
+      keyword: keyword.replace(/-/g, ' '),
+      src: `/audio/${keyword}/${file}`,
+    });
+    writeFileSync(process.cwd() + `/dist/audio/${keyword}/${file}`, readFileSync(process.cwd() + `/audio/${keyword}/${file}`, 'binary'), 'binary');
+  }
+}
+writeFileSync(
+  process.cwd() + '/public/audio-list.js',
+  'window.bjoernbuettner.audios=' + JSON.stringify(audios).replace(/"keyword":/g, 'keyword:').replace(/"src":/g, 'src:') + ';',
+  'utf8'
+);
 
 for(const file of readdirSync(process.cwd() + '/src', 'utf-8')) {
   if (file.endsWith('.scss')) {
@@ -39,7 +59,11 @@ for(const file of readdirSync(process.cwd() + '/src', 'utf-8')) {
 
 for(const file of readdirSync(process.cwd() + '/public', 'utf-8')) {
   if (file.endsWith('.js')) {
-    writeFileSync(process.cwd() + '/public/' + file, readFileSync(process.cwd() + '/public/' + file, 'utf8').replace(/export \{};/g, ''), "utf8");
+    writeFileSync(
+      process.cwd() + '/public/' + file,
+      readFileSync(process.cwd() + '/public/' + file, 'utf8').replace(/export \{};/g, ''),
+      "utf8"
+    );
   }
 }
 
@@ -48,7 +72,11 @@ for(const file of readdirSync(process.cwd() + '/public', 'utf-8')){
     try {
       const data = await minify(process.cwd() + '/public/' + file, config);
       if (data) {
-        writeFileSync(process.cwd() + '/dist/' + file, data);
+        if (file.endsWith('.js')) {
+          writeFileSync(process.cwd() + '/dist/' + file, '(async()=>{' + data+'})();');
+        } else {
+          writeFileSync(process.cwd() + '/dist/' + file, data);
+        }
       } else {
         console.error(`Failed to minify: ${file}`);
         writeFileSync(process.cwd() + '/dist/' + file, readFileSync(process.cwd() + '/public/' + file, 'binary'), 'binary');
@@ -80,7 +108,7 @@ for (const target of Object.keys(TO_MERGE)) {
     }
     out.push(readFileSync(`${process.cwd()}/dist/${file}`, 'utf8'));
   }
-  const content = out.join('\n');
+  const content = out.join('\n').replace(/\r/g, '').replace(/(\/\/# sourceMappingURL=.*)?\n{2,}/g, '\n');
   const contentHash = hash('md5', content, 'hex');
   const name = target.replace('##', contentHash);
   writeFileSync(`${process.cwd()}/dist/${name}`, content, 'utf8');
