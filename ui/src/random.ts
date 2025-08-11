0370a12bea9c4808f0d139ec42e528b65d5540bc
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-11 16:36:36 +0200

    use better randomizer if available

--- ui/src/random.ts
+++ ui/src/random.ts
@@ -1,12 +1,18 @@
 ((root) => {
+  const secureRandomUnit = (): number => {
+    if (window.crypto && window.crypto.getRandomValues) {
+      return (window.crypto.getRandomValues(new Uint32Array(1))[0]??0)/0x1_0000_0000;
+    }
+    return Math.random();
+  }
   root.randomFloat = (excludedMaximum: number): number => {
-    return Math.random() * excludedMaximum;
+    return secureRandomUnit() * excludedMaximum;
   }
   root.randomInt = (excludedMaximum: number): number => {
     return Math.floor(root.randomFloat(excludedMaximum));
   }
   root.randomBool = (): boolean => {
-    return Math.random() >= 0.5;
+    return secureRandomUnit() >= 0.5;
   }
   root.randomString = (list: string[]): string => {
     const randomised = [...list].sort(() => root.randomBool() ? 1 : -1)
