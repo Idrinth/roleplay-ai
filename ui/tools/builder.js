ad7125446f57cb67488ae172f9dea64bdde0777d
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-12 16:58:47 +0200

    fix error handling building (#244)

--- ui/tools/builder.js
+++ ui/tools/builder.js
@@ -68,6 +68,9 @@ for(const file of readdirSync(process.cwd() + '/public', 'utf-8')) {
     );
   }
 }
+if (!process.env.DESIRED_ROOT) {
+  process.env.DESIRED_ROOT = 'localhost';
+}
 
 for(const file of readdirSync(process.cwd() + '/public', 'utf-8')){
   if (file.endsWith('.js') || file.endsWith('.html') || file.endsWith('.css')) {
@@ -75,24 +78,24 @@ for(const file of readdirSync(process.cwd() + '/public', 'utf-8')){
       const data = await minify(process.cwd() + '/public/' + file, config);
       if (data) {
         if (file.endsWith('.js')) {
-          writeFileSync(process.cwd() + '/dist/' + file, '(async()=>{' + data.replace('###DESIRED_ROOT###', process.env.DESIRED_ROOT) + '})();');
+          writeFileSync(process.cwd() + '/dist/' + file, '(async()=>{' + data.replaceAll('###DESIRED_ROOT###', process.env.DESIRED_ROOT) + '})();');
         } else {
-          writeFileSync(process.cwd() + '/dist/' + file, data.replace('###DESIRED_ROOT###', process.env.DESIRED_ROOT));
+          writeFileSync(process.cwd() + '/dist/' + file, data.replaceAll('###DESIRED_ROOT###', process.env.DESIRED_ROOT));
         }
       } else {
         console.error(`Failed to minify: ${file}`);
         writeFileSync(
           process.cwd() + '/dist/' + file,
-          readFileSync(process.cwd() + '/public/' + file, 'utf8').replace('###DESIRED_ROOT###', process.env.DESIRED_ROOT),
+          readFileSync(process.cwd() + '/public/' + file, 'utf8').replaceAll('###DESIRED_ROOT###', process.env.DESIRED_ROOT),
           'utf8'
         );
       }
     } catch (e) {
       console.error(`Failed to minify ${file}: `, e);
-      writeFileSync(process.cwd() + '/dist/' + file, readFileSync(process.cwd() + '/public/' + file, 'binary'), 'binary');
+      writeFileSync(process.cwd() + '/dist/' + file, readFileSync(process.cwd() + '/public/' + file, 'utf8').replaceAll('###DESIRED_ROOT###', process.env.DESIRED_ROOT), 'utf8');
     }
   } else if(file.endsWith('.txt') || file.endsWith('.xml')) {
-    writeFileSync(process.cwd() + '/dist/'+file, readFileSync(process.cwd() +'/public/'+file, 'utf8').replace('###DESIRED_ROOT###', process.env.DESIRED_ROOT), 'utf8');
+    writeFileSync(process.cwd() + '/dist/'+file, readFileSync(process.cwd() +'/public/'+file, 'utf8').replaceAll('###DESIRED_ROOT###', process.env.DESIRED_ROOT), 'utf8');
   } else {
     writeFileSync(process.cwd() + '/dist/'+file, readFileSync(process.cwd() +'/public/'+file, 'binary'), 'binary');
   }
