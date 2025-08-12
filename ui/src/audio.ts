c238eecf3abb12dab96737ee8ab5ca434515b072
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-12 13:00:46 +0200

    display audio name and category (#233)

--- ui/src/audio.ts
+++ ui/src/audio.ts
@@ -1,14 +1,22 @@
 (async (root) => {
   root.getMatchingSongs = (keyword: string): string[] => {
+    const lowerCaseKeyword = keyword
+      .toLowerCase()
+      .replaceAll('-', ' ')
+      .replaceAll('_', ' ');
     const possibleAudios = [];
     for (const set of root.audios) {
-      if (keyword == set.keyword) {
+      if (lowerCaseKeyword == set.keyword) {
         possibleAudios.push(set.src);
       }
     }
     if (possibleAudios.length === 0) {
       for (const set of root.audios) {
-        if (keyword.includes(set.keyword)) {
+        const setKeyword = String(set.keyword ?? '')
+          .toLowerCase()
+          .replaceAll('-', ' ')
+          .replaceAll('_', ' ');
+        if (setKeyword.includes(lowerCaseKeyword) || lowerCaseKeyword.includes(set.keyword)) {
           possibleAudios.push(set.src);
         }
       }
@@ -22,10 +30,19 @@
   }
   audio.volume = 0;
   let isPlaying = false;
+  const setUcFirstInnerTextIfExists = (text: string|undefined, id: string) => {
+    const element = document.getElementById(id);
+    if (text && element) {
+      element.innerText = text
+        .split(" ")
+        .filter(textElement => !!textElement)
+        .map(textElement => textElement.substring(0, 1).toUpperCase() + textElement.substring(1))
+        .join(" ");
+    }
+  }
   const setRandomAudio = async() => {
-    const keywords = (document.getElementById("world") as null|HTMLInputElement)?.value.split(',').map(x => x.toLowerCase().trim()).filter(x => x !== '') ?? [];
     const possibleAudios = [];
-    for (const keyword in keywords) {
+    for (const keyword of root.getWorldKeywords()) {
       possibleAudios.push(...root.getMatchingSongs(keyword));
     }
     if (possibleAudios.length === 0) {
@@ -33,7 +50,11 @@
         possibleAudios.push(set.src);
       }
     }
-    audio.setAttribute('src', root.randomString(possibleAudios));
+    const src = root.randomString(possibleAudios);
+    const [,, category, name] = src.split('/');
+    setUcFirstInnerTextIfExists(category?.replaceAll('-', ' '), 'songcategory');
+    setUcFirstInnerTextIfExists(name?.split('.')[0]?.replaceAll('_', ' '), 'songname');
+    audio.setAttribute('src', src);
     if (!isPlaying) {
       try {
         audio.volume = 0.01;
