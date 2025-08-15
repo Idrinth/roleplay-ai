725b8e03b193d80885e7e785abc62748c52e0db7
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-15 08:41:31 +0200

    improve error logging for gguf export

--- beam-llm-training/shared/export_results.py
+++ beam-llm-training/shared/export_results.py
@@ -8,5 +8,5 @@ def export(name: str, model, tokenizer, dataset):
     try:
         model.push_to_hub_gguf(f"Idrinth/{name}ai-gguf", tokenizer, quantization_method="q4_k_m", token=os.environ["HUGGINGFACE_TOKEN"])
         print(f"Saved Model at Idrinth/{name}ai-gguf")
-    except Exception as e:
-        print(e)
+    except Exception as err:
+        print(f"Skipping GGUF export for Idrinth/{name}ai-gguf due to error: {err}")
