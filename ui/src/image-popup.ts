fa14ba7fcb5efe68be7971fcd358acd3e2da620d
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-16 13:43:42 +0200

    show video in modal for privacy reasons (#296)

--- ui/src/image-popup.ts
+++ ui/src/image-popup.ts
@@ -10,17 +10,33 @@
    picture.addEventListener('click', () => {
      const imgWrapper = document.createElement('div');
      const imgWrapperInner = document.createElement('div');
-     const img = document.createElement('img');
-     img.setAttribute('src', lastElementChild.getAttribute('src') || '');
-     img.setAttribute('alt', lastElementChild.getAttribute('alt') || '');
-     imgWrapper.appendChild(imgWrapperInner);
-     imgWrapperInner.appendChild(img);
      imgWrapper.setAttribute('title', 'click to close');
      imgWrapper.setAttribute('id', 'fullscreen-image');
      imgWrapper.addEventListener('click', () => {
        document.body.removeChild(imgWrapper);
-     })
+     });
+     imgWrapper.appendChild(imgWrapperInner);
      document.body.appendChild(imgWrapper);
+     if (picture.hasAttribute('data-video')) {
+       const frame = document.createElement('iframe');
+       frame.setAttribute('src', picture.getAttribute('data-video') || '');
+       frame.setAttribute('class', 'youtube-iframe');
+       frame.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share');
+       frame.setAttribute('allowfullscreen', 'allowfullscreen');
+       frame.setAttribute('referrerpolicy', 'strict-origin-when-cross-origin');
+       frame.addEventListener('click', (event: MouseEvent) => {
+         event.stopPropagation();
+       });
+       imgWrapperInner.appendChild(frame);
+       return;
+     }
+     const img = document.createElement('img');
+     img.setAttribute('src', lastElementChild.getAttribute('src') || '');
+     img.setAttribute('alt', lastElementChild.getAttribute('alt') || '');
+     img.addEventListener('click', (event: MouseEvent) => {
+       event.stopPropagation();
+     });
+     imgWrapperInner.appendChild(img);
    });
   }
  const pictures = document.getElementsByTagName('picture');
