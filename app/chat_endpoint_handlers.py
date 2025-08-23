58066741e1eb43240f77f99c29c662d6397d8eef
Platon Supranovich <psupra521@gmail.com>
2025-08-23 16:08:28 -0400

    removed repetitive code

--- app/chat_endpoint_handlers.py
+++ app/chat_endpoint_handlers.py
@@ -191,10 +191,9 @@ async def chat_message_internal(chat_id: str, user_id: str, action: Action, back
     medium_term_summary = get_from_redis(user_id,chat_id, "medium_summary")
     short_term_summary = get_from_redis(user_id,chat_id, "short_summary")
     world = get_from_redis(user_id, chat_id, "world", "[]")
-    world = ", ".join(json.loads(world))
+    world = json.loads(world)
     try:
-        world_keywords = json.loads(get_from_redis(user_id, chat_id, "world", "[]"))
-        for keyword in world_keywords:
+        for keyword in world:
             sql_connection.cursor().execute(
                 "INSERT INTO `chat_users`.`keywords` (word, count) VALUES (?, 1) "
                 "ON DUPLICATE KEY UPDATE count = count + 1;",
@@ -202,6 +201,7 @@ async def chat_message_internal(chat_id: str, user_id: str, action: Action, back
             )
     except Exception as e:
         log_exception(e, "chat_message_internal.keywords")
+    world = ", ".join(world)
     characters = []
     try:
         characters = list(mongo[mongodb_name(user_id, chat_id)]["characters"].find())
