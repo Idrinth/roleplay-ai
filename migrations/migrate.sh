ba107dccceff40136ba3fcd5996a12a55e7029c9
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-26 15:06:02 +0200

    Try to fix liquibase (#352)

--- migrations/migrate.sh
+++ migrations/migrate.sh
@@ -1,2 +1,1 @@
-liquibase changeLogSync --url="jdbc:mariadb://mariadb:3306/chat_users" --changeLogFile=sql-migrations.yml --username=root --password=example
 liquibase update --url="jdbc:mariadb://mariadb:3306/chat_users" --changeLogFile=sql-migrations.yml --username=root --password=example
