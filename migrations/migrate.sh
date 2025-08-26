9284850da26c576ca37cc7257f0d5d2f31aa047c
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-26 15:03:50 +0200

    relative path to migration file (#351)

--- migrations/migrate.sh
+++ migrations/migrate.sh
@@ -1,2 +1,2 @@
-liquibase changeLogSync --url="jdbc:mariadb://mariadb:3306/chat_users" --changeLogFile=/migrations/sql-migrations.yml --username=root --password=example
-liquibase update --url="jdbc:mariadb://mariadb:3306/chat_users" --changeLogFile=/migrations/sql-migrations.yml --username=root --password=example
+liquibase changeLogSync --url="jdbc:mariadb://mariadb:3306/chat_users" --changeLogFile=sql-migrations.yml --username=root --password=example
+liquibase update --url="jdbc:mariadb://mariadb:3306/chat_users" --changeLogFile=sql-migrations.yml --username=root --password=example
