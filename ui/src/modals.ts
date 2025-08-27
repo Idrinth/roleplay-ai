16b624c8698ed7d86a34dfe40a710e06c1930ece
Rajvardhan Singh Ranawat <146507318+Rajvardhan222@users.noreply.github.com>
2025-08-27 05:21:55 +0000

    refactor: adjust input focus handling in modal prompt

--- ui/src/modals.ts
+++ ui/src/modals.ts
@@ -11,7 +11,7 @@
     return new Promise(resolve => {
       const prmt = createModal(text, true);
       const input = document.createElement('input');
-      input.focus();
+      
       input.onkeyup = (event: KeyboardEvent) => {
         if (event.key === 'Enter') {
           if (!input.value) {
@@ -23,6 +23,7 @@
       }
       prmt.appendChild(input);
       input.value = defaultText;
+     
       prmt.appendChild(root.button('Send', 'Send changes', () => {
         if (!input.value) {
           return;
@@ -31,6 +32,7 @@
         resolve(input.value);
       }, true));
       document.body.appendChild(prmt);
+       input.focus();
     });
   }
   root.confirm = async (text: string): Promise<boolean> => {
