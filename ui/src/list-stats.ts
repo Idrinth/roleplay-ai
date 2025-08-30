402372b44e0f4cabe266a713289564f48a5ffdd5
Björn Büttner <Idrinth@users.noreply.github.com>
2025-08-30 12:32:09 +0200

    Fix statistics (#378)

--- ui/src/list-stats.ts
+++ ui/src/list-stats.ts
@@ -5,7 +5,7 @@
   }
   const statistics = document.getElementById('statistics');
   if (statistics) {
-    const isAllowedNumber = (a: unknown) => Number.isInteger(a) && Number.isNaN(a) && a as number > 0;
+    const isAllowedNumber = (a: unknown) => Number.isInteger(a) && !Number.isNaN(a) && a as number > 0;
     for (const statistic of Object.keys(data)) {
       if (root.isObjectWithProperty(data, statistic) && isAllowedNumber(data[statistic])) {
         const statisticElement = document.createElement('li');
