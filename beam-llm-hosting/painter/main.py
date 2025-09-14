0413c32db51db66af226c73feb6826f1725da5a5
Björn Büttner <Idrinth@users.noreply.github.com>
2025-09-14 18:57:19 +0200

    fix params

--- beam-llm-hosting/painter/main.py
+++ beam-llm-hosting/painter/main.py
@@ -55,7 +55,7 @@ def answer(context, **params):
     import base64
     image_pipe = context.on_start_value
     image = image_pipe(
-        prompt=params.get("description") + " " + params.get("world") + ", " + whitelist,
+        prompt=params.get("messages")[0] + ", " + whitelist,
         negative_prompt=blacklist,
         width=1664,
         height=928,
