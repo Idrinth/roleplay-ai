59e1670d99808c4fce5cbf04684b92d604efc6e2
Björn Büttner <Idrinth@users.noreply.github.com>
2025-09-14 19:21:26 +0200

    fix bugs found by starting a new chat

--- ui/src/document-upload.ts
+++ ui/src/document-upload.ts
@@ -9,13 +9,16 @@
     }
     const upload = async() => {
       const id = element.getAttribute('data-id');
-      if (id && element.value && element.getAttribute('data-raw') !== element.value && await root.confirm(`Do you want to save this modified ${resource}?`)) {
-        return await root.getFromAPI(`chat/${chatId}/${resource}s/${id}`, 'POST', await getBody(element));
+      if (id) {
+        if (element.value && element.getAttribute('data-raw') !== element.value && await root.confirm(`Do you want to save this modified ${resource}?`)) {
+          return await root.getFromAPI(`chat/${chatId}/${resource}s/${id}`, 'POST', await getBody(element));
+        }
+        return;
       }
       if (element.value && await root.confirm(`Do you want to save this new ${resource}?`)) {
         return await root.getFromAPI(`chat/${chatId}/${resource}s`, 'POST', await getBody(element));
       }
-      return undefined;
+      return;
     }
     await upload();
     element.parentElement?.removeChild(element);
