ec492b193f9ba360eb7b99e95ac4e31d6f5b91ad
Björn Büttner <Idrinth@users.noreply.github.com>
2025-09-15 09:50:22 +0200

    switch qwen image to 4bit

--- beam-llm-hosting/painter/main.py
+++ beam-llm-hosting/painter/main.py
@@ -23,7 +23,7 @@ def download_models():
         new_session=False,
     )
 
-    return DiffusionPipeline.from_pretrained("Qwen/Qwen-Image", torch_dtype=torch.bfloat16).to("cuda")
+    return DiffusionPipeline.from_pretrained("ovedrive/qwen-image-4bit", torch_dtype=torch.bfloat16).to("cuda")
 
 @endpoint(
     secrets=["HUGGINGFACE_TOKEN"],
