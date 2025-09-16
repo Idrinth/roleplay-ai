89889eca910644d0793aabc68dac58cb2a7c6c26
Björn Büttner <Idrinth@users.noreply.github.com>
2025-09-16 02:41:37 +0200

    fix image texts working in either case, not just exception

--- github-cache/main.py
+++ github-cache/main.py
@@ -46,33 +46,33 @@ def write_login(image: Image.Image, login: str, size: int):
     font_size = max(14, size // 5)
     try:
         font = ImageFont.truetype("DejaVuSans-Bold.ttf", font_size)
-    except:
+    except Exception:
         font = ImageFont.load_default()
 
-        text_width = draw.textlength(login, font=font)
+    text_width = draw.textlength(login, font=font)
 
-        if text_width > size:
-            mid = len(login) // 2
-            login_first = login[:mid]
-            login_second = login[mid:]
+    if text_width > size:
+        mid = len(login) // 2
+        login_first = login[:mid]
+        login_second = login[mid:]
 
-            text_width1 = draw.textlength(login_first, font=font)
-            text_x1 = size / 2 - text_width1 / 2
-            text_y1 = size + 2 
-            draw.text((text_x1, text_y1), login_first, font=font,
-                      fill=(0, 75, 0), stroke_width=2, stroke_fill=(255, 255, 255))
-            
-            text_width2 = draw.textlength(login_second, font=font)
-            text_x2 = size / 2 - text_width2 / 2
-            text_y2 = size + font_size + 2
-            draw.text((text_x2, text_y2), login_second, font=font, 
-                      fill=(0, 75, 0), stroke_width=2, stroke_fill=(255, 255, 255))
-            
-        else:
-            text_x = size / 2 - text_width / 2
-            text_y = size + 2
-            draw.text((text_x, text_y), login, font=font, 
-                      fill=(0, 75, 0), stroke_width=2, stroke_fill=(255, 255, 255))
+        text_width1 = draw.textlength(login_first, font=font)
+        text_x1 = size / 2 - text_width1 / 2
+        text_y1 = size + 2
+        draw.text((text_x1, text_y1), login_first, font=font,
+                  fill=(0, 75, 0), stroke_width=2, stroke_fill=(255, 255, 255))
+
+        text_width2 = draw.textlength(login_second, font=font)
+        text_x2 = size / 2 - text_width2 / 2
+        text_y2 = size + font_size + 2
+        draw.text((text_x2, text_y2), login_second, font=font,
+                  fill=(0, 75, 0), stroke_width=2, stroke_fill=(255, 255, 255))
+
+    else:
+        text_x = size / 2 - text_width / 2
+        text_y = size + 2
+        draw.text((text_x, text_y), login, font=font,
+                  fill=(0, 75, 0), stroke_width=2, stroke_fill=(255, 255, 255))
 
 async def download_avatar(session: aiohttp.ClientSession, avatar_url: str|None, login: str, avatar_size: int) -> Image.Image:
     if avatar_url:
