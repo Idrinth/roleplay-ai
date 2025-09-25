9a0e6f88db2c90865c735562ec9f6a2949f0b137
Björn Büttner <Idrinth@users.noreply.github.com>
2025-09-25 14:29:32 +0200

    make scan for last message less strict for fallbacks (#492)

--- beam-llm-hosting/shared/answer_from_model.py
+++ beam-llm-hosting/shared/answer_from_model.py
@@ -1,3 +1,5 @@
+from unittest import result
+
 import torch
 from typing import List, Dict
 
@@ -24,13 +26,24 @@ def answer_from_model(model, tokenizer, incoming_messages: List[Dict[str, str]],
         max_new_tokens=max_tokens,
         pad_token_id=tokenizer.eos_token_id,
     )
-    result = tokenizer.batch_decode(
+    llm_result = tokenizer.batch_decode(
         generated,
         skip_special_tokens=True,
         clean_up_tokenization_spaces=True,
     )[0]
 
-    outputs = result.split("\n assistant\n")
-    output = outputs[len(outputs) - 1]
-    outputs = output.split(incoming_messages[len(incoming_messages) - 1]["content"])
-    return outputs[len(outputs) - 1]
+    if llm_result == "":
+        raise Exception("No answer from model")
+    last_message = incoming_messages[len(incoming_messages) - 1]["content"].strip()
+    outputs = llm_result.split(last_message)
+    strip_position = 0
+    while len(outputs) == 1:
+        # something was changed in the last message, likely dot or comma placement corrections
+        strip_position += 1
+        if strip_position >= len(last_message) - 3:
+            raise Exception("Can't find last message in output.")
+        outputs = llm_result.split(last_message[strip_position:])
+    output = outputs[len(outputs) - 1].strip()
+    if output == "":
+        raise Exception("No answer from model")
+    return output
