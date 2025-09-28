03083007abc6924838077451d1e146f7a530f9be
Björn Büttner <Idrinth@users.noreply.github.com>
2025-09-28 09:03:08 +0200

    use larger context window model for hosting

--- beam-llm-training/shared/trainer_for_training.py
+++ beam-llm-training/shared/trainer_for_training.py
@@ -1,6 +1,9 @@
 def get_trainer(model, tokenizer, dataset):
-    from trl import SFTConfig, SFTTrainer
+    from trl import SFTConfig, SFTTrainer, DataCollatorForCompletionOnlyLM
     from .constants import MAX_SEQUENCE_LENGTH
+
+    response_template = ">>> Assistant:"
+    collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
 
     return SFTTrainer(
         model=model,
@@ -10,6 +13,7 @@ def get_trainer(model, tokenizer, dataset):
         max_seq_length=MAX_SEQUENCE_LENGTH,
         dataset_num_proc=2,
         packing=False,
+        data_collator=collator,
         args=SFTConfig(
             per_device_train_batch_size=2,
             gradient_accumulation_steps=4,
