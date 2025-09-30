dfe9847c1a60196dae5c4a44234ea0a80bf63c94
Björn Büttner <Idrinth@users.noreply.github.com>
2025-09-30 15:06:22 +0200

    Fix prompt template (#555)

--- beam-llm-hosting/shared/answer_from_model.py
+++ beam-llm-hosting/shared/answer_from_model.py
@@ -21,31 +21,37 @@ def answer_from_model(model, processor, incoming_messages: List[Dict[str, str]],
 
     tokenizer = processor.tokenizer
     template = r"""
-{%- if messages[0]['role'] == 'system' -%}
-  {%- set system_message = messages[0]['content'] if messages[0]['content'] is string else messages[0]['content']['text'] -%}
-  {%- set messages = messages[1:] -%}
-{%- else -%}
-  {%- set system_message = '' -%}
-{%- endif -%}
-{%- if system_message -%}
-{{ bos_token }}[INST] {{ system_message }}
+{{- bos_token }}
 
-{%- endif -%}
-{%- for message in messages -%}
-  {%- set content = message['content'] if message['content'] is string else message['content']['text'] -%}
-  {%- if message['role'] == 'user' -%}
-    {%- if loop.first and system_message -%}
-{{ content }} [/INST]
-    {%- else -%}
-{{ bos_token }}[INST] {{ content }} [/INST]
-    {%- endif -%}
-  {%- elif message['role'] == 'assistant' -%}
- {{ content }}{{ eos_token }}
-  {%- endif -%}
-{%- endfor -%}
-{%- if add_generation_prompt -%}
+{%- if messages[0]['role'] == 'system' %}
+    {%- if messages[0]['content'] is string %}
+        {%- set system_message = messages[0]['content'] %}
+    {%- else %}
+        {%- set system_message = messages[0]['content'][0]['text'] %}
+    {%- endif %}
+    {%- set loop_messages = messages[1:] %}
+{%- else %}
+    {{- raise_exception('System message required!') }}
+{%- endif %}
+{{- '[SYSTEM_PROMPT]' + system_message + '[/SYSTEM_PROMPT]' }}
 
-{%- endif -%}
+{%- for message in loop_messages %}
+    {%- if message['role'] == 'user' %}
+        {{- '[INST]' }}
+        {%- for block in message['content'] %}
+            {%- if block['type'] == 'text' %}
+                {{- block['text'] }}
+            {%- else %}
+                {{- raise_exception('Only text and image blocks are supported in message content!') }}
+            {%- endif %}
+        {%- endfor %}
+        {{- '[/INST]' }}
+    {%- elif message['role'] == 'assistant' %}
+        {{- message['content']['text'] + eos_token }}
+    {%- else %}
+        {{- raise_exception('Only user, assistant roles are supported!') }}
+    {%- endif %}
+{%- endfor %}
     """
     inputs = tokenizer.apply_chat_template(
         messages,
@@ -58,7 +64,7 @@ def answer_from_model(model, processor, incoming_messages: List[Dict[str, str]],
         pad_id = tokenizer.eos_token_id
 
     stop_ids = [tokenizer.eos_token_id]
-    for tok in ("[/INST]", "<|eot_id|>"):
+    for tok in ("[/INST]", "<|eot_id|>", "[/SYSTEM_PROMPT]"):
         tid = tokenizer.convert_tokens_to_ids(tok)
         if tid is not None and tid != tokenizer.unk_token_id:
             stop_ids.append(tid)
