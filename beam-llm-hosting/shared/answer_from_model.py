b50344df9d8fbbd404b76230f735b9549a26295c
Björn Büttner <Idrinth@users.noreply.github.com>
2025-10-01 14:27:28 +0200

    Fix templates to match the other way around (#570)

--- beam-llm-hosting/shared/answer_from_model.py
+++ beam-llm-hosting/shared/answer_from_model.py
@@ -21,27 +21,28 @@ def answer_from_model(model, processor, incoming_messages: List[Dict[str, str]],
 
     tokenizer = processor.tokenizer
     template = r"""
-    {{- bos_token }}
+{{- bos_token }}
 
-    {%- if messages[0]['role'] == 'system' %}
-        {%- set system_message = messages[0]['content']['text'] %}
-        {%- set loop_messages = messages[1:] %}
+{%- if messages[0]['role'] == 'system' %}
+    {%- set system_message = messages[0]['content']['text'] %}
+    {%- set loop_messages = messages[1:] %}
+{%- else %}
+    {{- raise_exception('System message required!') }}
+{%- endif %}
+{{- '[SYSTEM_PROMPT]' + system_message + '[/SYSTEM_PROMPT]' }}
+
+{%- for message in loop_messages %}
+    {%- if message['role'] == 'user' %}
+        {{- '[INST]' }}
+        {{- message['content']['text'] }}
+        {{- '[/INST]' }}
+    {%- elif message['role'] == 'assistant' %}
+        {{- message['content']['text'] + eos_token }}
     {%- else %}
-        {{- raise_exception('System message required!') }}
+        {{- raise_exception('Only user, assistant roles are supported!') }}
     {%- endif %}
-    {{- system_message + '\n' }}
-
-    {%- for message in loop_messages %}
-        {%- if message['role'] == 'user' %}
-            {{- '>>> User: ' + message['content']['text'] + '\n' }}
-        {%- elif message['role'] == 'assistant' %}
-            {{- '>>> Assistant: ' + message['content']['text'] + eos_token + '\n' }}
-        {%- else %}
-            {{- raise_exception('Only user, assistant roles are supported!') }}
-        {%- endif %}
-    {%- endfor %}
-    {{- '>>> Assistant: ' }}
-"""
+{%- endfor %}
+    """
     inputs = tokenizer.apply_chat_template(
         messages,
         add_generation_prompt=True,
