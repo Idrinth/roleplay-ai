4d80bdde8e9597da48fe05db43af8f0965f4fd19
Björn Büttner <Idrinth@users.noreply.github.com>
2025-10-02 10:56:36 +0200

    adjust quant. for gguf format

--- beam-llm-training/shared/export_results.py
+++ beam-llm-training/shared/export_results.py
@@ -6,7 +6,7 @@ def export(name: str, model, tokenizer, dataset):
     model.push_to_hub(f"Idrinth/{name}ai", tokenizer, quantization_method="q4_k_m", token=os.environ["HUGGINGFACE_TOKEN"])
     print(f"Saved Model at Idrinth/{name}ai")
     try:
-        model.push_to_hub_gguf(f"Idrinth/{name}ai-gguf", tokenizer, quantization_method="q4_k_m", token=os.environ["HUGGINGFACE_TOKEN"])
+        model.push_to_hub_gguf(f"Idrinth/{name}ai-gguf", tokenizer, quantization_method="q8_0", token=os.environ["HUGGINGFACE_TOKEN"])
         print(f"Saved Model at Idrinth/{name}ai-gguf")
     except Exception as err:
         print(f"Skipping GGUF export for Idrinth/{name}ai-gguf due to error: {err}")
